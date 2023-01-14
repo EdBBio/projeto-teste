b58578100d6a0a4706fb1d182d8f572c97ae304b
EdBBio <Edson151413@gmail.com>
2023-01-14 18:51:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# projeto teste
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of projeto teste is to ...
+
---
+++ configurações.R
@@ -0,0 +1,26 @@
+# Pacote
+
+library(usethis)
+
+# Iniciando a Configuração
+
+use_git_config(user.name = "EdBBio",
+               user.email = "Edson151413@gmail.com")
+
+# Configurando o Token
+
+create_github_token()
+
+edit_r_environ()
+
+# Iciando o Git
+
+git_sitrep()
+
+use_git()
+
+use_github()
+
+# Criando o readme.md
+
+use_readme_md()
